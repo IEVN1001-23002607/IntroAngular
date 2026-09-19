8f08d501083a476bdfe476c6fd0653f2a1879d1c
IEVN1001-23002607 <angelfarnandez9@gmail.com>
2026-09-19 10:02:23 -0600

    pipe 19/09/26

--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IHeroes } from '../heroes';
 
 @Component({
   selector: 'app-heroes-list',
@@ -6,4 +7,45 @@ import { Component } from '@angular/core';
   //styleUrl: './heroes-list.css',
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+
+  imageWith:number=40;
+  imageMargin:number=2;
+  muestraImagen:boolean=true;
+  listFilter:string='';
+showImage():void{
+  this.muestraImagen=!this.muestraImagen;
+}
+
+  heroes:IHeroes[]=[
+    {
+      imagen:'https://dragonball-api.com/characters/goku_normal.webp',
+      nombre:'Goku',
+      description:'El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla. Sin embargo, el caer por un barranco le proporcionó un brutal golpe que si bien casi lo mata, este alteró su memoria y anuló todos los instintos violentos de su especie, lo que lo hizo crecer con un corazón puro y bondadoso, pero conservando todos los poderes de su raza. No obstante, en la nueva continuidad de Dragon Ball se establece que él fue enviado por sus padres a la Tierra con el objetivo de sobrevivir a toda costa a la destrucción de su planeta por parte de Freeza. Más tarde, Kakarot, ahora conocido como Son Goku, se convertiría en el príncipe consorte del monte Fry-pan y líder de los Guerreros Z, así como el mayor defensor de la Tierra y del Universo 7, logrando mantenerlos a salvo de la destrucción en innumerables ocasiones, a pesar de no considerarse a sí mismo como un héroe o salvador',
+      race:'Saiyan',
+      ki:'60000000'
+    },
+    {
+      imagen:'https://dragonball-api.com/characters/vegeta_normal.webp',
+      nombre:'Vegeta',
+      description:'Príncipe de los Saiyans, inicialmente un villano, pero luego se une a los Z Fighters. A pesar de que a inicios de Dragon Ball Z, Vegeta cumple un papel antagónico, poco después decide rebelarse ante el Imperio de Freeza, volviéndose un aliado clave para los Guerreros Z. Con el paso del tiempo llegaría a cambiar su manera de ser, optando por permanecer y vivir en la Tierra para luchar a su lado contra las inminentes adversidades que superar. Junto con Piccolo, él es de los antiguos enemigos de Goku que ha evolucionando al pasar de ser un villano y antihéroe, a finalmente un héroe a lo largo del transcurso de la historia, convirtiéndose así en el deuteragonista de la serie',
+      race:'Saiyan',
+      ki:'54000000'
+    },
+    {
+      imagen:'https://dragonball-api.com/characters/BuuGordo_Universo7.webp',
+      nombre:'Majin buu',
+      description:'También conocido como Boo original, es la forma original y pura de Majin-Boo, y la última forma de Boo que aparece en Dragon Ball Z',
+      race:'Saiyan',
+      ki:'8 billones'
+    },
+    {
+      imagen:'https://dragonball-api.com/characters/Beerus_DBS_Broly_Artwork.webp',
+      nombre:'Bills',
+      description:'Dios de la Destrucción Beerus, conocido también como Beers, o Bills en Hispanoamérica e inicialmente en España[1], es un personaje que fue introducido en la película Dragon Ball Z: La batalla de los dioses, donde es el antagonista principal de la película, y que aparece en el manga y anime de Dragon Ball Super como un personaje principal. Ocupa el puesto de Dios de la Destrucción de todo el Universo 7 siendo el lugar donde se desarrolla la historia de Dragon Ball',
+      race:'Saiyan',
+      ki:'102 billones'
+    }
+    
+  ]
+}
